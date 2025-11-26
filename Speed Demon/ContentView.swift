abaf0e90ed67f0934b5e2d355d1fe37a6a37a3ba
Craig Little <craig@nickelrose.com.au>
2025-11-26 13:55:56 +1100

    Initial Commit

---
+++ Speed Demon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speed Demon
+//
+//  Created by Craig Little on 26/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speed Demon/Speed_DemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speed_DemonApp.swift
+//  Speed Demon
+//
+//  Created by Craig Little on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Speed_DemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
